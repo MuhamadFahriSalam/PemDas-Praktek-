bf22f795f76079e06aa5c3f7d48a3f25639176d8
MuhamadFahriSalam <muhamadfahrisalam059@gmail.com>
2024-03-29 00:58:58 +0700

    fix

---
+++ Main(Pak_Yana).py
@@ -0,0 +1,21 @@
+def choose():
+    print ("Rencana setelah lulus sekolah?\n1.Kuliah\n2.Kerja")
+
+    input("Masukkan pilihan: ")
+    if (input == 1):
+        print("Kuliah jurusan apa?\n1.Teknik Industri n\2.Kedokteran")
+        input("Masukkan pilihan")
+        if ():
+            print ("Kuliah di Jurusan Teknik Industri")
+        else :
+         print ("Kuliah di Jurusan Kedokteran")
+        
+    else :
+        print("Kerja di Industri apa?\n1.pertambangan \n2.Teknologi")
+        input("Masukkan Pilihan: ")
+        if ():
+         print ("Kerja di Industri Pertambangan")
+        else :
+         print("Kerja di Industri Teknologi")
+ 
+choose()   
--- Pertemuan2/intepedensi.py
+++ Pertemuan2/intepedensi.py
@@ -27,79 +27,79 @@ if __name__ == '__main__':
     
     
 # TUGAS 2
-def main():
-    bahasa = 'pyhon'
-    versi = 3
+# def main():
+#     bahasa = 'pyhon'
+#     versi = 3
     
-    print(bahasa, versi) 
-    print(bahasa + ' ' + str(versi))
-    print('%s %d' % (bahasa, versi))
-    print('{} {}' .format(bahasa, versi))
-    print('{0} {1}'.format(bahasa, versi))
+#     print(bahasa, versi) 
+#     print(bahasa + ' ' + str(versi))
+#     print('%s %d' % (bahasa, versi))
+#     print('{} {}' .format(bahasa, versi))
+#     print('{0} {1}'.format(bahasa, versi))
     
-if __name__== '__main__':
-    main()
+# if __name__== '__main__':
+#     main()
 
 
 # TUGAS 3
-def main():
-    alas = 4.0
-    tinggi = 5.0
-    luas = alas * tinggi / 2
+# def main():
+#     alas = 4.0
+#     tinggi = 5.0
+#     luas = alas * tinggi / 2
     
-    print('Menghitung luas segitiga')
-    print(f'Alas\t: {alas}')
-    print(f'Tinggi\t: {tinggi}')
-    print(f'Luas/t: {luas}')
+#     print('Menghitung luas segitiga')
+#     print(f'Alas\t: {alas}')
+#     print(f'Tinggi\t: {tinggi}')
+#     print(f'Luas/t: {luas}')
     
-if __name__ == '__main__' :
-    main()
+# if __name__ == '__main__' :
+#     main()
 
 
 
 # TUGAS 4
-def main():
-    print('Masukkan data diri Anda')
-    nama = input('Nama\t: ')
-    alamat = input('Alamat\t: ')
-    telephone = input('No telephone\t: ')
+# def main():
+#     print('Masukkan data diri Anda')
+#     nama = input('Nama\t: ')
+#     alamat = input('Alamat\t: ')
+#     telephone = input('No telephone\t: ')
     
-    print('\nData diri: %s, %s, %s, %' (nama, alamat, telephone))
+#     print('\nData diri: %s, %s, %s, %' (nama, alamat, telephone))
     
-    print('\ntype(nama\t: %s)' % type(nama))
-    print('\ntype(alamat\t: %s)' % type(alamat))
-    print('\ntype(telephone\t: %s)' % type(telephone))
+#     print('\ntype(nama\t: %s)' % type(nama))
+#     print('\ntype(alamat\t: %s)' % type(alamat))
+#     print('\ntype(telephone\t: %s)' % type(telephone))
    
-if __name__ == '__main__' :
-    main()
+# if __name__ == '__main__' :
+#     main()
  
  
  
 #  TUGAS 5
-def main():
-    print('Menghitung luas persegi panjang')
-    panjang = int(input('Panjang\t: '))
-    lebar = int(input('Lebar\t: '))
+# def main():
+#     print('Menghitung luas persegi panjang')
+#     panjang = int(input('Panjang\t: '))
+#     lebar = int(input('Lebar\t: '))
     
-    luas = panjang * lebar
+#     luas = panjang * lebar
     
-    print('\nluas = %d' % luas)
+#     print('\nluas = %d' % luas)
     
-if __name__ == '__main__' :
-    main()
+# if __name__ == '__main__' :
+#     main()
 
 
 
 # TUGAS 6
-import math 
+# import math 
 
-def main():
-    print('Menghitung luas lingkaran')
-    radius = float(input('Jari-jari\t: '))
+# def main():
+#     print('Menghitung luas lingkaran')
+#     radius = float(input('Jari-jari\t: '))
     
-    luas = math.pi * (radius ** 2)
+#     luas = math.pi * (radius ** 2)
     
-    print('\nLuas = %.2f' % luas)
+#     print('\nLuas = %.2f' % luas)
     
-if __name__ == '__main__' :
-    main()
+# if __name__ == '__main__' :
+#     main()
