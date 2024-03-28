3b382c6f2499b0e2c81c56fb385e49b22b947d01
MuhamadFahriSalam <muhamadfahrisalam059@gmail.com>
2024-03-29 01:18:21 +0700

    fix

--- Pak_Yana(Teori)/Main(Pak_Yana).py
+++ Pak_Yana(Teori)/Main(Pak_Yana).py
@@ -1,21 +1,30 @@
 def choose():
-    print ("Rencana setelah lulus sekolah? \n1.Kuliah \n2.Kerja")
-
-    input("Masukkan pilihan: ")
-    if (input == 1):
-        print("Kuliah jurusan apa? \n1.Teknik Industri n\2.Kedokteran")
-        input("Masukkan pilihan")
-        if ():
-            print ("Kuliah di Jurusan Teknik Industri")
-        else :
-         print ("Kuliah di Jurusan Kedokteran")
+    print("Rencana setelah lulus sekolah? \n1. Kuliah \n2. Kerja")
+    pilihan_utama = input("Masukkan pilihan: ")
+    
+    if pilihan_utama == '1':
+        print("Kuliah jurusan apa? \n1. Teknik Industri \n2. Kedokteran")
+        pilihan_kuliah = input("Masukkan pilihan: ")
+        if pilihan_kuliah == '1':
+            print("Kuliah di Jurusan Teknik Industri")
+        elif pilihan_kuliah == '2':
+            print("Kuliah di Jurusan Kedokteran")
+        else:
+            print("Pilihan tidak valid.")
         
-    else :
-        print("Kerja di Industri apa? \n1.pertambangan \n2.Teknologi")
-        input("Masukkan Pilihan: ")
-        if ():
-         print ("Kerja di Industri Pertambangan")
-        else :
-         print("Kerja di Industri Teknologi")
- 
-choose()   
+    elif pilihan_utama == '2':
+        print("Kerja di Industri apa? \n1. Pertambangan \n2. Teknologi")
+        pilihan_kerja = input("Masukkan Pilihan: ")
+        if pilihan_kerja == '1':
+            print("Kerja di Industri Pertambangan")
+        elif pilihan_kerja == '2':
+            print("Kerja di Industri Teknologi")
+        else:
+            print("Pilihan tidak valid.")
+    else:
+        print("Pilihan tidak valid.")
+
+choose()
+
+
+
