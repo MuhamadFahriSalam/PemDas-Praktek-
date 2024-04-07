92610bb0f560f77b874add958a4158b048ea0945
MuhamadFahriSalam <muhamadfahrisalam059@gmail.com>
2024-04-08 01:35:16 +0700

    fix

--- Pak_Yana(Teori)/Tugas1.py
+++ Pak_Yana(Teori)/Tugas1.py
@@ -1,36 +1,35 @@
 def gaji_karyawan():
-    # Define gaji dasar untuk setiap golongan
-    GAJI_DASAR = {'A': 1000000, 'B': 1500000, 'C': 2000000}
-    # Define persentase tunjangan untuk setiap golongan
-    TUNJANGAN_PERS = {'A': 0.05, 'B': 0.15, 'C': 0.25}
-    # Define gaji lembur per jam untuk setiap golongan
-    GAJI_LEMBUR_PER_JAM = {'A': 20000, 'B': 25000, 'C': 30000}
-    # Dapatkan input dari pengguna
-    nama = input("Nama: ")
+    GAJI_DASAR = {'A': 1000000, 'B': 1500000, 'C': 2000000} # Define gaji dasar untuk setiap golongan
+    TUNJANGAN_PERS = {'A': 0.05, 'B': 0.15, 'C': 0.25} # Define persentase tunjangan untuk setiap golongan
+    GAJI_LEMBUR_PER_JAM = {'A': 20000, 'B': 25000, 'C': 30000} # Define gaji lembur per jam untuk setiap golongan
+     # Dapatkan input dari pengguna
+    nama = input("Nama: ") 
     masa_kerja = int(input("Masa Kerja: "))
     golongan = input("Golongan: ").upper()
     total_jam_kerja = int(input("Total Jam Kerja: "))
-    # Hitung gaji dasar berdasarkan golongan
-    gaji = GAJI_DASAR[golongan]
-    # Hitung tunjangan berdasarkan golongan
-    tunjangan = TUNJANGAN_PERS[golongan] * gaji
-    # Hitung gaji lembur jika berlaku
-    if total_jam_kerja > 40 * 4:  # 40 jam * 4 minggu
+    gaji = GAJI_DASAR[golongan] # Hitung gaji dasar berdasarkan golongan
+    tunjangan = TUNJANGAN_PERS[golongan] * gaji # Hitung tunjangan berdasarkan golongan
+    if total_jam_kerja > 40 * 4:  # Hitung gaji lembur jika berlaku # 40 jam * 4 minggu
         jam_lembur = total_jam_kerja - 40 * 4
         gaji_lembur = GAJI_LEMBUR_PER_JAM[golongan] * jam_lembur
     else:
         gaji_lembur = 0
-    # Hitung total gaji
-    total_gaji = gaji + tunjangan + gaji_lembur
+    total_gaji = gaji + tunjangan + gaji_lembur # Hitung total gaji
     # Cetak hasil
-    print("\nDATA PEGAWAI")
-    print("Nama: ", nama)
-    print("Masa Kerja: ", masa_kerja, "tahun")
-    print("Golongan: ", golongan)
-    print("Total Jam Kerja: ", total_jam_kerja, "jam")
-    print("\nPERHITUNGAN GAJI")
-    print("Gaji Pokok: ", gaji)
-    print("Tunjangan: ", tunjangan)
-    print("Gaji Lembur: ", gaji_lembur)
-    print("Total Gaji: ", total_gaji)
+    print('-' *25) 
+    print("{:^20}".format("DATA PEGAWAI"))
+    print('-' *25)
+    print("{:<15}: {:>2}".format("Nama", nama))
+    print("{:<15}: {:>2}".format("Masa Kerja", masa_kerja, "tahun"))
+    print("{:<15}: {:>2}".format("Golongan", golongan))
+    print("{:<13}: {:>3}".format("Total Jam Kerja", total_jam_kerja, "jam"))
+    print('-' *25)
+    print("{:^20}".format("PERHITUNGAN GAJI"))
+    print('-' *25)
+    print("{:<12}: {:>8,}".format("Gaji Pokok", gaji))
+    print("{:<12}: {:>8,}".format("Tunjangan", tunjangan))
+    print("{:<12}: {:>8,}".format("Gaji Lembur", gaji_lembur))
+    print('-' *25)
+    print("{:<12}: {:>8,}".format("Total Gaji", total_gaji))
+    print('-' *25)
 gaji_karyawan()
